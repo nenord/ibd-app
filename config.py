e7852964d9e67753ca8a408e0e95a1eef140b968
nenord <nradanovic@zoho.com>
2020-05-17 14:16:20 +0100

    safer handling of sensitive env variables, replaced yelp token

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
             'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    HEADERS = os.environ.get('HEADERS') or {"Authorization": "Bearer mC8NHf9eLhgkR3K5fGJejTvsi8BVTeiJnjxjfJ-okjxGK539sWW4ULcMkMCC99GWiQvv8dIPSzHyB6e4Ru44Y56-EPIsBuWN6tMuT1L7YENa2aMH7eS4iCvD0JKIXnYx"}
+    HEADERS = os.environ.get('HEADERS')
     ENDPOINTS = os.environ.get('ENDPOINTS') or 'https://api.yelp.com/v3/graphql'
