1bec833bf7e7b06531653ee3df297028aa308858
nenord <nradanovic@zoho.com>
2020-05-23 11:08:53 +0100

    added flas-moments to indicate whena post was made

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,6 +12,8 @@ bootstrap = Bootstrap(app)
 login = LoginManager(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+moment = Moment(app)
+
 login.login_view = 'login'
 
 from app import routes, models
